b2c8c6cc9779b73db39137a663f89e3d6f3be476
admin <admin@YD-11>
2022-07-04 17:50:32 +0900

    0704 숫자맞추기

---
+++ P0704/src/co/edu/loop/Example3.java
@@ -0,0 +1,31 @@
+package co.edu.loop;
+
+import java.util.Scanner;
+
+public class Example3 {
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		boolean isTrue = true;
+		int sum = 0; // sum의 값이 100을 넘어서면 종료
+		do {
+			System.out.println("임의의 값을 입력하세요.");
+			int num = scan.nextInt();
+			sum += num;
+			// 누적값이 100을 초과하기 전의 값을 출력
+
+			if (sum > 100) {
+				sum -= num;
+				System.out.println("반복문을 종료합니다.");
+				isTrue = false;// or break;
+			}
+
+		} while (isTrue);
+		// 누적값이 100을 초과하기 전의 값을 출력해보자
+		System.out.println("sum의 누적값: " + sum);
+		System.out.println("end of program.");
+
+		scan.close();
+	}
+
+}
---
+++ P0704/src/co/edu/loop/Example4.java
@@ -0,0 +1,30 @@
+package co.edu.loop;
+
+import java.util.Scanner;
+
+public class Example4 {
+	public static void main(String[] args) {
+		// while 반복문의 종료조건.
+		// 1~100 임의의 수 생성
+		Scanner scan = new Scanner(System.in);
+
+		int temp = (int) (Math.random() * 100) + 1;
+		System.out.println("값을 입력하세요.");
+		int inputVal = scan.nextInt();
+		while (true) {
+
+			if (inputVal == temp) {
+				System.out.println("맞췄습니다.");
+			} else if (inputVal > temp) {
+				System.out.println(inputVal + "보다 작습니다.");
+				break;
+			} else if (inputVal < temp) {
+				System.out.println(inputVal + "보다 큽니다.");
+				break;
+			}
+			System.out.println("end of pregram.");
+			// scan.close();
+
+		}
+	}
+}
